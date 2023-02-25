7d7c7e654778847403c7c15f4356049bad4eaee4
Tony Gilkerson <tgilkerson@miletwo.us>
2023-02-24 22:06:26 -0500

    add metrics

--- cmd/serial/main.go
+++ cmd/serial/main.go
@@ -3,33 +3,28 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/tarm/serial"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func main() {
-	
-	//
-	// Get environment Variables
-	//
-	device, exists := os.LookupEnv("MALONE_DEVICE")
 
-	if !exists {
+var postDeliveryCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+			Name: "post_delivered_count",
+			Help: "No of times post has been delivered",
+	},
+)
 
-		err := fmt.Errorf("MALONE_DEVICE is required")
-		log.Fatal(err)
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-
-	}
-
-
+func monitorSerial(device string) {
 	for {
 		var err error
 		var n int
 
-		log.Printf("[main] Open serial device: %v", device)
+		log.Printf("[monitorSerial] Open serial device: %v", device)
 		// c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
 		// c := &serial.Config{Name: "/dev/ttys002", Baud: 115200}
 		c := &serial.Config{Name: device, Baud: 115200}
@@ -43,7 +38,7 @@ func main() {
 		// 	log.Fatal(err)
 		// }
 
-		log.Println("[main] Read...")
+		log.Println("[monitorSerial] Read...")
 		buf := make([]byte, 128)
 		n, err = s.Read(buf)
 		if err != nil {
@@ -51,6 +46,40 @@ func main() {
 		}
 		log.Printf("%q", buf[:n])
 
+		//
+		// For now consider any input as post delivery 
+		//
+		postDeliveryCounter.Inc()
+
 	}
+}
+
+func main() {
+
+	//
+	// Get environment Variables
+	//
+	device, exists := os.LookupEnv("MALONE_DEVICE")
+
+	if !exists {
+
+		err := fmt.Errorf("MALONE_DEVICE is required")
+		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+
+	}
+	go monitorSerial(device)
+
+	//
+	// Register metric
+	//
+	prometheus.MustRegister(postDeliveryCounter)
+
+	//
+	// Server up API endpoints
+	//
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":8080", nil)
 
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,19 @@ module github.com/tonygilkerson/malone-sidecar
 
 go 1.19
 
-require github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+require (
+	github.com/prometheus/client_golang v1.14.0
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+)
 
-require golang.org/x/sys v0.5.0 // indirect
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
