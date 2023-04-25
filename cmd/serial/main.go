427b52f3053f6ef43fddbe9456354489e8fb41e4
Tony Gilkerson <tgilkerson@miletwo.us>
2023-04-24 22:24:00 -0400

    watch for metrics counters

--- cmd/serial/main.go
+++ cmd/serial/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/tonygilkerson/marty/pkg/marty"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/tarm/serial"
@@ -41,27 +43,66 @@ func main() {
 
 func serialServer(serialPort string) {
 
-	//
 	// Define a counter to keep track of the number of times the post has been delivered
-	// The device in the field will send a signal when it detect post delivery
-	// A device attached to the serial port of one of the nodes will receive the signal
-	// We will want to increment the counter when the signal is received
-	var postDeliveryCounter = prometheus.NewCounter(
+	var mbxPostDeliveryCounter = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "post_delivered_count",
+			Name: "mvx_post_delivered_count",
 			Help: "No of times post has been delivered",
 		},
 	)
-	prometheus.MustRegister(postDeliveryCounter)
+	prometheus.MustRegister(mbxPostDeliveryCounter)
+
+	// Define a counter to keep track of the number of times a car has arrived
+	var mbxArrivedCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "mbx_arrived_count",
+			Help: "No of times a car has arrived",
+		},
+	)
+	prometheus.MustRegister(mbxArrivedCounter)
+
+	//
+	// Define a counter to keep track of the number of times a car has departed
+	//
+	var mbxDepartedCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "mbx_departed_count",
+			Help: "No of times a car has departed",
+		},
+	)
+	prometheus.MustRegister(mbxDepartedCounter)
+
+	//
+	// Define a counter to keep track of the number of times a car has departed
+	//
+	var mbxErrorCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "mbx_error_count",
+			Help: "No of times an error has occurred counting cars",
+		},
+	)
+	prometheus.MustRegister(mbxErrorCounter)
+
+	//
+	// Define a counter to keep track of the number of false alarms while counting cars  
+	//
+	var mbxFalseAlarmCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "mbx_false_count",
+			Help: "No of times an error has occurred counting cars",
+		},
+	)
+	prometheus.MustRegister(mbxFalseAlarmCounter)
 
 	//
 	// Open the serial device
 	//
 	log.Printf("Open serial port: %v", serialPort)
+
 	// Device is something like "/dev/ttyUSB0"
 	cfg := &serial.Config{Name: serialPort, Baud: 115200}
 	port, err := serial.OpenPort(cfg)
-	// log.Printf("[DEBUG] port: %v, err: %v", port, err)
+	
 	if err != nil {
 		log.Fatalf("error trying to open serial port %q. %v", serialPort, err)
 	}
@@ -70,22 +111,42 @@ func serialServer(serialPort string) {
 	//
 	// Monitor the serial port forever
 	//
+	buf := make([]byte, 128)
 	log.Printf("Start read loop for serial port: %q...", serialPort)
+
 	for {
+
 		var err error
 		var n int
+		var msg string
 
-		buf := make([]byte, 128)
 		n, err = port.Read(buf)
 		if err != nil {
-			log.Fatalf("error trying to read serial port %q. %v", serialPort, err)
+			log.Fatalf("error trying to read serial port %q. %v\n", serialPort, err)
 		}
-		log.Printf("%q", buf[:n])
+		msg = string(buf[:n])
+		log.Printf("Rx: %v\n", msg)
+		
+		switch msg {
+		case string(marty.Arrived):
+			mbxArrivedCounter.Inc()
+			log.Println("increment mbxArrivedCounter")
 
-		//
-		// For now consider any input as post delivery
-		//
-		postDeliveryCounter.Inc()
+		case string(marty.Departed):
+			mbxDepartedCounter.Inc()
+			log.Println("increment mbxDepartedCounter")
 
+		case string(marty.Error):
+			mbxErrorCounter.Inc()
+			log.Println("increment mbxErrorCounter")
+
+		case string(marty.FalseAlarm):
+			mbxFalseAlarmCounter.Inc()
+			log.Println("increment mbxFalseAlarmCounter")
+
+		default:
+			log.Printf("I don't know what to do with: %s\n", msg )
+		}
+		
 	}
 }
